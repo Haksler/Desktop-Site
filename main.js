cb4bb497d410263e4604ca18f454e7e9cfa43990
Haksler <bslowik4@gmail.com>
2022-03-16 21:36:37 +0100

    Commit 1 - landing page
    First commit of my project

---
+++ main.js
@@ -0,0 +1,6 @@
+function sizechangemainpage () {
+    document.getElementById('navbar').classList.add("animation")
+    }
+function sizechangemainpage2 () {
+     document.getElementById('navbar').classList.remove("animation")
+}
